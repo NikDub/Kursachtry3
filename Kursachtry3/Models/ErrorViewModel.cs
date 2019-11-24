43a2128f1a16a9db66bf9f33cde53c1e1510c05f
NikDub <37387164+NikDub@users.noreply.github.com>
2019-11-25 00:18:39 +0300

    Добавьте файлы проекта.

---
+++ Kursachtry3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Kursachtry3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
